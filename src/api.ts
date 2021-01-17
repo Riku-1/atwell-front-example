7431dbb604d6a38eae1a4cc6a49409211d030527
Riku-1 <osaka.riku@gmail.com>
2021-01-17 19:29:35 +0900

    Feature(#16):Changed to add query from and to

--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,19 @@
 import { BACKEND_HOST, BACKEND_PORT } from "./config";
 import { Tweet } from "./domain/tweet";
+import { formatDate } from "./utils/formatter";
 
 const fetchData = async (url: string) => {
   return await fetch(url);
 };
 
 export const fetchTweets = async (start: Date, end: Date) => {
-  const url = BACKEND_HOST + ":" + BACKEND_PORT + "/tweets";
-  return fetchData(url).then((res) =>
+  const url = new URL(BACKEND_HOST + ":" + BACKEND_PORT + "/tweets");
+  url.search = new URLSearchParams([
+    ["from", formatDate(start)],
+    ["to", formatDate(end)],
+  ]).toString();
+
+  return fetchData(url.toString()).then((res) =>
     res
       .json()
       .then((json: []) =>
